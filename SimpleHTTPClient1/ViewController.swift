058ec0857f881cd6f9654bddffa31dc14f68a91f
ITLoaner <itloaner@itloaner-hs.local>
2020-10-06 18:20:50 -0700

    Initial Commit

---
+++ SimpleHTTPClient1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleHTTPClient1
+//
+//  Created by ITLoaner on 10/6/20.
+//  Copyright © 2020 ITLoaner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
